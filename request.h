e4bb6cbb2a5129db2ad26647f857d9ba43a1539b
Arjun Rajesh <arjun110503@gmail.com>
2026-02-18 12:18:00 -0600

    Created request.h file with the Request struct, which contains the incoming IP address, outgoing IP address, time variable to note length required to process that request, and the job type of that request (default was chosen to be processing).

---
+++ request.h
@@ -0,0 +1,12 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+#include <string>
+
+struct Request {
+    std::string incoming_IP;
+    std::string outgoing_IP;
+    int time_track;
+    char job_type;
+
+    Request() : incoming_IP("0.0.0.0"), outgoing_IP("0.0.0.0"), time_track(0), job_type('p') {}
+};
