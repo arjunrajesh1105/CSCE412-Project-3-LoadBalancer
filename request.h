034c083e11ef05ee39f9f841f3c44a889e601e96
Your Full Name <your.email@example.com>
2026-02-19 16:40:58 -0600

    Added comments to the request.h file.

--- request.h
+++ request.h
@@ -1,13 +1,13 @@
-#ifndef REQUEST_H
-#define REQUEST_H
+#ifndef Request_H
+#define Request_H
 #include <string>
 
 struct Request {
-    std::string incoming_IP;
-    std::string outgoing_IP;
-    int time_track;
-    char job_type;
+    std::string incoming_IP; // This expresses the incoming IP address of the request
+    std::string outgoing_IP; // This expresses the outgoing IP address of the request
+    int time_track; // This is to track time for a request to be fully processed
+    char job_type; // This will be to express if the job type is processing (p) or streaming (s)
 
-    Request() : incoming_IP("0.0.0.0"), outgoing_IP("0.0.0.0"), time_track(0), job_type('p') {}
+    Request() : incoming_IP("0.0.0.0"), outgoing_IP("0.0.0.0"), time_track(0), job_type('p') {} // This is the default constructor for the struct Request
 };
 #endif
