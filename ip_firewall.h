a79ae41844ccc8a707a176ef56e4d0f9e7936b25
Your Full Name <your.email@example.com>
2026-02-22 17:41:12 -0600

    Added a firewall class that will act as the ip blocker to detect if incoming requests' ip addresses are malicious or safe.

---
+++ ip_firewall.h
@@ -0,0 +1,17 @@
+#ifndef IP_Firewall_H
+#define IP_Firewall_H
+#include <string>
+using std::string;
+
+class IP_Firewall
+{
+    public:
+        string firewallAnalysis(string IP_address); // This functions checks if the IP address of the incoming request is a threat or not (threat or safe)
+
+    private:
+        string threat_IP_octets[5] = {"73.", "97.", "91.", "23.", "185."}; // This vector contains the beginning octets of dangerous IP addresses that should be blocked
+        int IP_block_count = 0; // This expresses the total number of blocked IP addresses and will update whenever an IP address is blocked
+};
+
+
+#endif
