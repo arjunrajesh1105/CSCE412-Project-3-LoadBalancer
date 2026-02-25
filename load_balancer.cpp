51e97d7be069c0a98f1d3cd1f1c8da6f60c6270c
Your Full Name <your.email@example.com>
2026-02-24 18:16:25 -0600

    I created the load_balancer.cpp file that contains the empty functions without the logic yet to be implemented.

---
+++ load_balancer.cpp
@@ -0,0 +1,32 @@
+#include "load_balancer.h"
+using std::string;
+
+Load_Balancer::Load_Balancer(int server_total) : clock_cycle(0), requests_finshed(0), requests_blocked(0), rest_period(0)
+{
+    
+}
+
+void totalCycleHandle()
+{
+
+}
+
+void addServer()
+{
+    
+}
+
+void removeServer()
+{
+
+}
+
+Request createRandomRequest()
+{
+
+}
+
+string randomIPGenerator()
+{
+
+}
