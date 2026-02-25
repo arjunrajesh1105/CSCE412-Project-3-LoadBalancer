d7fdd116b13cbd56878ddbe8bdad410603c3b6bb
Your Full Name <your.email@example.com>
2026-02-25 16:23:38 -0600

    Updated main.cpp file to complement loadbalancer functionality.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "request.h"
+#include "load_balancer.h"
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
 
-int main() {
-    Request current_request;
-    std::cout << "Incoming IP address: " << current_request.incoming_IP << std::endl;
-    std::cout << "Outgoing IP address: " << current_request.outgoing_IP << std::endl;
-    std::cout << "Time: " << current_request.time_track << std::endl;
-    std::cout << "Job Type: " << current_request.job_type << std::endl;
-    return 0;
+int main() 
+{
+    int server_input;
+    int cycle_input;
+    int clock_cycle_track = 0;
+    srand(time(0));
+
+    cout << "Please input the amount of web servers you would like: " << endl;
+    cin >> server_input;
+    cout << "Please input the amount of clock cycles you would like: " << endl;
+    cin >> cycle_input;
+    Load_Balancer loadbalancer(server_input);
+
+    while (clock_cycle_track < cycle_input)
+    {
+        loadbalancer.totalCycleHandle();
+        clock_cycle_track++;
+    }
 }
