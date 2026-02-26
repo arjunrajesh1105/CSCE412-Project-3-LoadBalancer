13770ff7fdf4dc73ca0693e59c8f28e79a532c0b
Your Full Name <your.email@example.com>
2026-02-25 21:36:09 -0600

    Updated load_balancer.cpp with logic filled out on most functions.

--- load_balancer.cpp
+++ load_balancer.cpp
@@ -1,32 +1,64 @@
 #include "load_balancer.h"
+#include <string>
 using std::string;
+using std::to_string;
 
 Load_Balancer::Load_Balancer(int server_total) : clock_cycle(0), requests_finished(0), requests_blocked(0), rest_period(0)
 {
+    int server_track = 0;
+    while (server_track < server_total)
+    {
+        web_servers.push_back(Web_Server(server_track));
+        server_track++;
+    }
 
+    int request_track = 0;
+    while (request_track < (server_total * 100))
+    {
+        req_queue.push(createRandomRequest());
+        request_track++;
+    }
 }
 
-void totalCycleHandle()
+void Load_Balancer::totalCycleHandle()
 {
 
 }
 
-void addServer()
+void Load_Balancer::addServer()
 {
-    
+    int last_index = web_servers.size() - 1;
+    web_servers.push_back(Web_Server(last_index + 1));
 }
 
-void removeServer()
+void Load_Balancer::removeServer()
 {
-
+    int last_index = web_servers.size() - 1;
+    web_servers.pop_back();
 }
 
-Request createRandomRequest()
+Request Load_Balancer::createRandomRequest()
 {
-
+    Request curr_request;
+    curr_request.incoming_IP = randomIPGenerator();
+    curr_request.outgoing_IP = randomIPGenerator();
+    curr_request.time_track = (rand() % 76) + 2;
+    if((rand() % 2) == 0)
+    {
+        curr_request.job_type = 'p';
+    }
+    else
+    {
+        curr_request.job_type = 's';
+    }
+    return curr_request;
 }
 
-string randomIPGenerator()
+string Load_Balancer::randomIPGenerator()
 {
-
+    int octet1 = rand() % 256;
+    int octet2 = rand() % 256;
+    int octet3 = rand() % 256;
+    int octet4 = rand() % 256;
+    return to_string(octet1) + "." + to_string(octet2) + "." + to_string(octet3) + "." + to_string(octet4);
 }
